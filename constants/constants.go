af2e9fd9018703a800d11324cd416d675b424d68
Saharsh Shukla <shukla.saharsh7.work@gmail.com>
2023-01-30 14:17:26 +0530

    chore: doc

--- constants/constants.go
+++ constants/constants.go
@@ -4,22 +4,27 @@ var BaseURL = "https://api.openai.com"
 var apiKey = ""
 var orgId = ""
 
+// SetApiKey sets the API key for the OpenAI API
 func SetApiKey(key string) {
 	apiKey = key
 }
 
+// SetOrgId sets the organization ID for the OpenAI API
 func SetOrgId(id string) {
 	orgId = id
 }
 
+// GetApiKey returns the API key for the OpenAI API
 func GetApiKey() string {
 	return apiKey
 }
 
+// GetOrgId returns the organization ID for the OpenAI API
 func GetOrgId() string {
 	return orgId
 }
 
+// GetToken returns the Bearer token for the OpenAI API
 func GetToken() string {
 	return "Bearer " + apiKey
 }
--- constants/endpoints.go
+++ constants/endpoints.go
@@ -2,7 +2,9 @@ package constants
 
 type endpoints map[string]RequestData
 
+// openAiEndpoints is a map of all the endpoints for the OpenAI API
 var openAiEndpoints = endpoints{
+	// To list all models
 	"listModels": {
 		Endpoint: "/v1/models",
 		Method:   "GET",
@@ -10,12 +12,15 @@ var openAiEndpoints = endpoints{
 	},
 }
 
+// dalleEndpoints is a map of all the endpoints for the Dalle API
 var dalleEndpoints = endpoints{
+	// To generate images using a prompt
 	"generateImages": {
 		Endpoint: "v1/images/generations",
 		Method:   "POST",
 		Body:     nil,
 	},
+	// To edit images using a mask and an image with a prompt
 	"editImages": {
 		Endpoint:    "v1/images/edits",
 		Method:      "POST",
@@ -24,17 +29,24 @@ var dalleEndpoints = endpoints{
 	},
 }
 
+// RequestData is a struct that contains the data required to make a request
 type RequestData struct {
-	Endpoint    string
-	Method      string
-	Body        interface{}
+	// Endpoint is the endpoint of the request
+	Endpoint string
+	// Method is the method of the request GET/POST/PATCH etc
+	Method string
+	// Body accepts an interface{} as the body of the request
+	Body interface{}
+	// ContentType of the request is optional, if not provided it will be set to application/json
 	ContentType string
 }
 
+// GetOpenAIEndpoint returns the endpoint data for the OpenAI API
 func GetOpenAIEndpoint(endpointType string) RequestData {
 	return openAiEndpoints[endpointType]
 }
 
+// GetDalleEndpoint returns the endpoint data for the Dalle API
 func GetDalleEndpoint(endpointType string) RequestData {
 	return dalleEndpoints[endpointType]
 }
